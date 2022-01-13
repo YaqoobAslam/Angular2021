1273845ab2a4da4689b523550da0cc999ac6fd7e
Yaqoob <happyyaqoob@gmail.com>
2022-01-13 00:34:18 -0800

    reactive form control commit

---
+++ reactive-form-example/reactiveform-app/src/app/profile-editor/profile-editor.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup } from '@angular/forms';
+@Component({
+  selector: 'app-profile-editor',
+  templateUrl: './profile-editor.component.html',
+  styleUrls: ['./profile-editor.component.css']
+})
+export class ProfileEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  profileForm = new FormGroup({
+  firstName: new FormControl(''),
+  lastName: new FormControl(''),
+  address:  new FormGroup({
+    street: new FormControl(''),
+    city: new FormControl(''),
+    state: new FormControl(''),
+    zip: new FormControl('')
+  })
+  });
+  onSubmit(){
+    console.warn(this.profileForm.value);
+    
+  }
+}
